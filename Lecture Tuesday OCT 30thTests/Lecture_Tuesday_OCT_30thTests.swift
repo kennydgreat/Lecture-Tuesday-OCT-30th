933393db75b1908de111c560126f286a164ea7b4
Kenneth Uyabeme <uyabeme@oa-s231-11.ddi.sheridanc.on.ca>
2018-10-30 11:31:53 -0400

    Initial Commit

---
+++ Lecture Tuesday OCT 30thTests/Lecture_Tuesday_OCT_30thTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lecture_Tuesday_OCT_30thTests.swift
+//  Lecture Tuesday OCT 30thTests
+//
+//  Created by Kenneth Uyabeme on 10/30/18.
+//  Copyright © 2018 Kenneth Uyabeme. All rights reserved.
+//
+
+import XCTest
+@testable import Lecture_Tuesday_OCT_30th
+
+class Lecture_Tuesday_OCT_30thTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
